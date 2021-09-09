5529da756c0218fa64384733771613723f521d82
kugiyasan <kugiyasan@users.noreply.github.com>
2021-09-09 14:40:49 -0400

    Completed exercice.py

--- exercice.py
+++ exercice.py
@@ -4,32 +4,37 @@
 
 import math
 
+
 def square_root(a: float) -> float:
-    return 0.0
+    return a ** 0.5
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a * a
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    angle = angle_degs + angle_mins / 60 + angle_secs / 3600
+    return angle * math.pi / 180
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    angle_degs = angle_rads * 180 / math.pi
+    angle_mins = angle_degs * 60 % 60
+    angle_secs = angle_mins * 60 % 60
+    return int(angle_degs), int(angle_mins), int(angle_secs)
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) * 5 / 9
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature * 1.8 + 32
 
 
 def main() -> None:
@@ -39,14 +44,21 @@ def main() -> None:
 
     print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
-    
+    print(
+        f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(100, 2, 45)}"
+    )
+    print(
+        f"Conversion de 180 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}"
+    )
+
     degrees, minutes, seconds = to_degrees(1.0)
-    print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
+    print(
+        f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes"
+    )
 
     print(f"Conversion de 100 Celsius en Farenheit: {to_farenheit(100.0)}")
     print(f"Conversion de 451 Farenheit en Celsius: {to_celsius(451.0)}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
